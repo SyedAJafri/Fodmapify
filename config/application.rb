eb62539ed5f6d18ca5715977ff5184aa490217ff
SyedAJafri <syedjafri99@gmail.com>
2016-07-17 17:25:21 -0400

    Using ActionController::API to create an api based class

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.api_only = true
+
 module Fodmapify
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
