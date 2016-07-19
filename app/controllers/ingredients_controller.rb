dcc4da95bda3117f99ba57fd8d53e3d0e2cd549b
SyedAJafri <syedjafri99@gmail.com>
2016-07-18 21:14:13 -0400

    Created IngredientsController with create, update, and index

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,11 +1,42 @@
 class IngredientsController < ApplicationController
-  before_action only: [:show, :create]
+  before_action only: [:index, :show, :create]
 
-  # GET /ingredient
+  # GET /ingredients
   def index
     render json: Ingredient.all
   end
 
+  # GET /ingredients/[:id]
   def show
+    render json: Ingredient.find(params[:id])
+  end
+
+  # POST /ingredients
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+
+    if @ingredient.save
+      render json: @ingredient, status: :created, location: @ingredient
+    else
+      render json: @ingredient.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /ingredients/[:id]
+  def update
+    @ingredient = Ingredient.new(ingredient_params)
+
+    if @ingredient.update(ingredient_params)
+      render json: @ingredient
+    else
+      render json: @ingredient.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name, :fodmap)
+  end
 
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,4 @@
 class Ingredient < ActiveRecord::Base
-  attr_accessible :name, :fodmap
-
   validates :name, :presence =>  true, :length => { :maximum => 200 }
   validates :fodmap, :presence =>  true, inclusion: { in: [true, false] }, exclusion: { in: [nil] }
 end
