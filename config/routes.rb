548e6b23106e7697f202f0729405f3e60c9cb543
SyedAJafri <syedjafri99@gmail.com>
2016-07-20 22:20:57 -0400

    Created index, and create ingredient routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'ingredients#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get '/ingredients' => 'ingredients#index'
+  get '/ingredients/:id' => 'ingredients#show'
+  post '/ingredients' => 'ingredients#create'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
