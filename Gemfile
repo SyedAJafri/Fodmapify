c9475338cb7773b73dbbc5b9b6ba21ddb856eb52
SyedAJafri <syedjafri99@gmail.com>
2016-08-08 20:44:54 -0400

    Configured angular 2 to talk to ruby on rails. Created basic FE that makes a request to /ingredients.

--- Gemfile
+++ Gemfile
@@ -17,6 +17,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 #For heroku
 gem 'rails_12factor', group: :production
 
+gem 'rack-cors'
+
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
@@ -26,6 +28,7 @@ gem 'rails_12factor', group: :production
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,18 @@ module Fodmapify
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.api_only = true
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Explicitly add the 'node_modules' directory
+    config.assets.paths << Rails.root.join('node_modules')
+
+    # Allow cors
+    config.middleware.insert_before 0, 'Rack::Cors' do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+
   end
 end
+
